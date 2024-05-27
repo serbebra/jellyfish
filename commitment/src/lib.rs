17fa90bc0ed46849a1135f21312518a0714faa83
sergo <154074169+serbebra@users.noreply.github.com>
2024-05-27 03:37:41 +0200

    Update lib.rs

--- commitment/src/lib.rs
+++ commitment/src/lib.rs
@@ -1,41 +1,40 @@
-// Copyright (c) 2022 Espresso Systems (espressosys.com)
-// This file is part of the Jellyfish library.
-
-// You should have received a copy of the MIT License
-// along with the Jellyfish library. If not, see <https://mit-license.org/>.
-//! Trait definition for cryptographic commitment scheme
 #![no_std]
 
-use ark_std::{borrow::Borrow, fmt::Debug, hash::Hash, UniformRand};
+use ark_std::{borrow::Borrow, fmt::Debug, hash::Hash, error::Error, UniformRand};
 
-/// A glorified [`bool`] that leverages compile lints to encourage the caller to
-/// use the result.
-///
-/// Intended as the return type for verification of proofs, signatures, etc.
-/// Recommended for use in the nested [`Result`] pattern: see <https://sled.rs/errors>.
+/// Result type for cryptographic verification to improve code semantics and error handling.
 type VerificationResult = Result<(), ()>;
 
+/// Defines a cryptographic commitment scheme with generic types for inputs, outputs, and randomness.
+///
+/// This trait provides methods for creating and verifying commitments. Implementations must specify
+/// how commitments are generated and verified, including handling of optional randomness for blinding.
 pub trait CommitmentScheme {
-    /// Input to the commitment
+    /// The type of input data used to create a commitment.
     type Input;
-    /// The type of output commitment value
+    /// The type of the commitment output, must support cloning, hashing, and comparison.
     type Output: Clone + Debug + PartialEq + Eq + Hash;
-    /// The type of the hiding/blinding factor
+    /// The type for the randomness used in the commitment process, supporting uniform random generation.
     type Randomness: Clone + Debug + PartialEq + Eq + UniformRand;
-    /// Error type
-    type Error: ark_std::error::Error;
+    /// The type of error that may be returned by commitment operations.
+    type Error: Error;
 
-    /// Commit algorithm that takes `input` and blinding randomness `r`
-    /// (optional for hiding commitment schemes), outputs a commitment.
+    /// Creates a commitment from the given `input` and an optional `randomness`.
+    ///
+    /// The `randomness` is optional and can be used to blind the commitment if required by the scheme.
+    /// Returns either the commitment or an error if the operation cannot be completed.
     fn commit<T: Borrow<Self::Input>>(
         input: T,
-        r: Option<&Self::Randomness>,
+        randomness: Option<&Self::Randomness>,
     ) -> Result<Self::Output, Self::Error>;
 
-    /// Verify algorithm that output `Ok` if accepted, or `Err` if rejected.
+    /// Verifies a commitment against the original `input` and the provided `randomness`.
+    ///
+    /// This method checks if the given `commitment` corresponds to the `input` with the optional `randomness`.
+    /// It returns `Ok` wrapped in `VerificationResult` if the verification is successful, otherwise an error.
     fn verify<T: Borrow<Self::Input>>(
         input: T,
-        r: Option<&Self::Randomness>,
-        comm: &Self::Output,
+        randomness: Option<&Self::Randomness>,
+        commitment: &Self::Output,
     ) -> Result<VerificationResult, Self::Error>;
 }
